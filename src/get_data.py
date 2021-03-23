dd3a601253a99696ae2bebb15e2a0080edbca398
snar2019 <narain.narain21@gmail.com>
2021-03-23 11:08:28 -0400

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -1,27 +1,25 @@
 ## read params
 ## process
 ## return dataframe
-
 import os
 import yaml
 import pandas as pd
 import argparse
-
-
 
 def read_params(config_path):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
     return config
 
-
-
 def get_data(config_path):
     config = read_params(config_path)
-    #print(config)
+    # print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     return df
+
+
+
 
 
 if __name__=="__main__":
